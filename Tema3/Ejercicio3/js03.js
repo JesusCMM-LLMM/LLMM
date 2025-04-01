ca253a3cb0d262640ddae54da1df174f5c6d4001
JesusCMM <jesuscmoramesa@gmail.com>
2025-04-01 13:16:05 +0200

    Add files via upload

---
+++ Tema3/Ejercicio3/js03.js
@@ -0,0 +1,7 @@
+function cambiaCoche(nombre) {
+    document.getElementById('miImagen').src = nombre;   
+}
+
+function cambiaSpidey(nombre) {
+    document.getElementById('miSpidey').src = nombre;   
+}
